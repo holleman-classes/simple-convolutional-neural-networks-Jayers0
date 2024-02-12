ffff043cbe2f3dab62dadcc0ee0f81249ffb82f8
Jayers0 <132713143+Jayers0@users.noreply.github.com>
2024-02-11 21:11:13 -0500

    Add files via upload

--- hw2_complete.py
+++ hw2_complete.py
@@ -30,6 +30,8 @@ def build_model50k():
   return model
 
 # no training or dataset construction should happen above this line
+model50k = build_model50k()
+
 if __name__ == '__main__':
 
   ########################################
@@ -42,7 +44,7 @@ if __name__ == '__main__':
   train_images, test_images = train_images / 255.0, test_images / 255.0
 
   ########################################
-  ep = 25
+  ep = 50
   ## Build and train model 1
   model1 = build_model1()
   # compile and train model 1.
@@ -62,7 +64,7 @@ if __name__ == '__main__':
   model3.fit(train_images, train_labels, epochs=ep, validation_data=(test_images, test_labels))
   print("Model 3 training completed.")
 
-  model50k = build_model50k()
+  
   model50k.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
   model50k.fit(train_images, train_labels, epochs=ep, validation_data=(test_images, test_labels))
   model50k.save("best_model.h5")
--- model3.py
+++ model3.py
@@ -2,68 +2,65 @@ import tensorflow as tf
 
 def build_model3():
     inputs = tf.keras.layers.Input(shape=(32, 32, 3))
-    x = inputs
     
     # First convolutional block
-    x = tf.keras.layers.Conv2D(32, (3, 3), strides=(1, 1), padding='same', activation='relu')(x)
+    x = tf.keras.layers.Conv2D(32, (3, 3), strides=(2, 2), padding='same', activation='relu')(inputs)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2))(x)
     
     # Second convolutional block
-    x = tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu')(x)
+    x = tf.keras.layers.Conv2D(64, (3, 3), strides=(2, 2), padding='same', activation='relu')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.Conv2D(64, (3, 3), padding='same', activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+    
+    # Skip connection after the second convolutional block
+    skip_connection1 = tf.keras.layers.Conv2D(128, (2, 2), strides=(2, 2), padding='same')(x)
+    skip_connection1 = tf.keras.layers.BatchNormalization()(skip_connection1)
     
     # Third convolutional block
-    x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
+    x = tf.keras.layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', activation='relu')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
-    x = tf.keras.layers.MaxPooling2D((2, 2))(x)
+    
+    x = tf.keras.layers.Add()([x, skip_connection1])
+    
+    # Skip connection after the third convolutional block
+    skip_connection2 = tf.keras.layers.Conv2D(128, (2, 2), strides=(1, 1), padding='same')(x)
+    skip_connection2 = tf.keras.layers.BatchNormalization()(skip_connection2)
     
     # Fourth convolutional block
     x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
+    
+    x = tf.keras.layers.Add()([x, skip_connection2])
+    
+    # Fifth convolutional block
     x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Dropout(0.2)(x)
     
-    # Max pooling and flatten
-    x = tf.keras.layers.MaxPooling2D((4, 4))(x)
+    # Skip connection after the 5th convolutional block
+    skip_connection3 = tf.keras.layers.Conv2D(128, (1, 1), strides=(1, 1), padding='same')(x)
+    skip_connection3 = tf.keras.layers.BatchNormalization()(skip_connection3)
+    
+    # Sixth convolutional block
+    x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    
+    # Skip connection after the 6th convolutional block
+    x = tf.keras.layers.Add()([x, skip_connection3])
+    x = tf.keras.layers.Activation('relu')(x)
+    
+    # 7th convolutional block
+    x = tf.keras.layers.Conv2D(128, (3, 3), padding='same', activation='relu')(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    
+    # MaxPooling layer
+    x = tf.keras.layers.MaxPooling2D(pool_size=(4, 4), strides=(4, 4), padding='same')(x)
+    
+    # Flatten layer
     x = tf.keras.layers.Flatten()(x)
     
     # Dense layers
     x = tf.keras.layers.Dense(128, activation='relu')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    
-    # Adding three additional Add() layers
-    shortcut = x
-    x = tf.keras.layers.Dense(128, activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Add()([shortcut, x])
-    
-    shortcut = x
-    x = tf.keras.layers.Dense(128, activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Add()([shortcut, x])
-    
-    shortcut = x
-    x = tf.keras.layers.Dense(128, activation='relu')(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Add()([shortcut, x])
-    
     outputs = tf.keras.layers.Dense(10, activation='softmax')(x)
 
-    model = tf.keras.Model(inputs=inputs, outputs=outputs)
-    return model
+    model3 = tf.keras.Model(inputs=inputs, outputs=outputs)
+    return model3
